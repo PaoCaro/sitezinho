083e5b26114b83f73e775b40c7364a01bbe840ef
!!PãoCaro!! <42713181+PaoCaro@users.noreply.github.com>
2019-04-30 19:25:17 +0100

    js folder

---
+++ js/hover.js
@@ -0,0 +1,8 @@
+function abrir() {
+    document.getElementById("HoverSeiLa1").style.width = "250px";
+    document.getElementById("HoverSeiLa").style.marginLeft = "250px";
+  }
+    function fechar() {
+      document.getElementById("HoverSeiLa1").style.width = "0px";
+      document.getElementById("HoverSeiLa").style.marginLeft = "0px";
+    }
