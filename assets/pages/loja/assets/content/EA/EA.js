710fd2ffdb6fefc780094c91cbfdcc3965c86e75
PaoCaro <tfrcampos@gmail.com>
2019-05-14 17:33:01 +0100

    EA Acess

--- assets/pages/loja/assets/content/EA/EA.js
+++ assets/pages/loja/assets/content/EA/EA.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Check if element is scrolled into view
     function isScrolledIntoView(elem) {
       var docViewTop = $(window).scrollTop();
       var docViewBottom = docViewTop + $(window).height();
@@ -9,9 +8,4 @@ $(document).ready(function() {
   
       return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
-    // If element is scrolled into view, fade it in
-    //$(window).scroll(function() {
-      //$('.scroll-animations .animated').each(function() {
-        //if (isScrolledIntoView(this) === true) {
-          //$(this).addClass('fadeInLeft');
         });
